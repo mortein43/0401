7c74959ec917a1e4aca5c12a785d5cb23cd8280f
mortein43 <mortein43@gmail.comm>
2023-10-24 11:15:32 +0300

    Добавьте файлы проекта.

---
+++ 0401.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <future>
+
+
+int loadFile(const std::string& filename) {
+    std::ifstream file(filename);
+    if (!file.is_open()) {
+        std::cerr<< std::endl << "Неможливо відкрити файл: " << filename << std::endl;
+        return 0;
+    }
+
+    std::string content((std::istreambuf_iterator<char>(file)), std::istreambuf_iterator<char>());
+    file.close();
+    return content.size();
+}
+
+int main() {
+    system("chcp 1251>nul");
+    std::vector<std::string> files = { "file1.txt", "file2.txt", "file3.txt", "file4.txt", "file5.txt" }; 
+
+    std::vector<std::future<int>> futures;
+    for (const auto& file : files) {
+        futures.emplace_back(std::async(std::launch::async, loadFile, file));
+    }
+
+    int totalSize = 0;
+    for (int i = 0; i < files.size(); ++i) {
+        int fileSize = futures[i].get();
+        totalSize += fileSize;
+        std::cout << "Розмір файлу " << files[i] << " - " << fileSize << " символів." << std::endl;
+    }
+    std::cout << "Загальний розмір усіх файлів - " << totalSize << " символів." << std::endl;
+
+    return 0;
+}
